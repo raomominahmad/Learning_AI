62ea964cf15160294e3004f45d7193c0b2a7bf9e
raomominahmad <258462334+raomominahmad@users.noreply.github.com>
2026-08-14 21:56:49 +0500

    Adding threading multiprocessing and GIL topic

---
+++ python_learning/12_threads_concurrency/03_gil_threading.py
@@ -0,0 +1,31 @@
+# Global interppreter Lock (GIL)
+# In normal CPython, the GIL is a lock that allows only one thread 
+# at a time to execute Python bytecode within a process.
+import threading
+import time
+
+
+def brew_chai():
+    print(f"{threading.current_thread().name} started brewing")
+    count = 0
+
+    for _ in range(100_000_000):
+        count += 1
+
+    print(f"{threading.current_thread().name} finished brewing...")
+
+
+thread1 = threading.Thread(target=brew_chai, name="Barista-1")
+thread2 = threading.Thread(target=brew_chai, name="Barista-2")
+
+start = time.time()
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
+
+end = time.time()
+
+print(f"total time taken {end - start:.2f} seconds ")
